8c724008dbc5d19b4a4b8106c2194de47c9fb405
Ariel <ariel.amriou@epitech.eu>
2026-02-09 13:41:51 +0100

    feat: add of cmake

--- CMakeFiles/Makefile.cmake
+++ CMakeFiles/Makefile.cmake
@@ -45,4 +45,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/NanoTekSpice.dir/DependInfo.cmake"
+  "CMakeFiles/tests_run.dir/DependInfo.cmake"
   )
---
+++ CMakeFiles/tests_run.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests_run.dir/tests/tests_init.cpp.o"
+  "CMakeFiles/tests_run.dir/tests/tests_init.cpp.o.d"
+  "tests_run"
+  "tests_run.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

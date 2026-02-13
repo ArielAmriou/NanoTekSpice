d766035ea1918ccd52027325d620301156b6b0e0
Ariel <ariel.amriou@epitech.eu>
2026-02-13 11:06:46 +0100

    fix: change of 4069 pins order

--- src/Components/AComponent.cpp
+++ src/Components/AComponent.cpp
@@ -41,19 +41,19 @@ nts::Tristate nts::AComponent::compute(std::size_t pin) {
 void nts::AComponent::simulate(std::size_t tick) noexcept {
     auto iter = this->_pins.begin();
     auto end = this->_pins.end();
-
-    this->_lastUpdateTick = tick;
+    
     for (std::size_t i = 0; iter != end; ++i, ++iter) {
         std::optional<nts::Connection> &con = iter.base()->getConnection();
         if (iter.base()->getMode() == nts::Mode::InputMode && con.has_value()){
-            if (con.value().getComponent().getLastUpdateTick() < tick)
+            if (this->_name != con.value().getComponent().getName())
                 con.value().getComponent().simulate(tick);
             auto value =
-                con.value().getComponent().compute(con.value().getPin());
+            con.value().getComponent().compute(con.value().getPin());
             this->_pins[i].setValue(value);
         }
     }
     if (this->_lastUpdateTick < tick) {
         this->simulateComponent();
+        this->_lastUpdateTick = tick;
     }
 }
--- src/Components/C6Not.cpp
+++ src/Components/C6Not.cpp
@@ -14,9 +14,12 @@ nts::C6Not::C6Not() : AComponent("4069") {
 }
 
 void nts::C6Not::simulateComponent(void) {
-    for(std::size_t i = 0; i < this->_nbPins; i += 2)
+    for(std::size_t i = 0; i < this->_nbPins / 2; i += 2)
         this->_pins[i + 1].setValue(
             nts::BasicOperation::notOperation(this->_pins[i].getValue()));
+    for(std::size_t i = this->_nbPins / 2; i < this->_nbPins; i += 2)
+        this->_pins[i].setValue(
+            nts::BasicOperation::notOperation(this->_pins[i + 1].getValue()));
 }
 
 const std::vector<nts::Pin> nts::C6Not::_defaultPins = {
@@ -26,10 +29,11 @@ const std::vector<nts::Pin> nts::C6Not::_defaultPins = {
     nts::Mode::OutputMode,
     nts::Mode::InputMode,
     nts::Mode::OutputMode,
-    nts::Mode::InputMode,
+
     nts::Mode::OutputMode,
     nts::Mode::InputMode,
     nts::Mode::OutputMode,
     nts::Mode::InputMode,
     nts::Mode::OutputMode,
+    nts::Mode::InputMode,
 };

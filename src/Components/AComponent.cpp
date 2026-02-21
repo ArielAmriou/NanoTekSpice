6e1f53509d2b992237f79e06ce1cf9f7928ba66c
Pierrick <pierrick@epitech.eu>
2026-02-21 23:58:55 +0100

    fix: the AComponents.cpp file that was delete by error

---
+++ src/Components/AComponent.cpp
@@ -0,0 +1,76 @@
+/*
+** EPITECH PROJECT, 2026
+** NanoTekSpice
+** File description:
+** AComponent
+*/
+
+#include "AComponent.hpp"
+#include "Pin.hpp"
+
+void nts::AComponent::setLink(std::size_t pin,
+    nts::IComponent &other, std::size_t otherPin)
+{
+    if (pin >= _nbPins)
+        throw NoSuchPin();
+    Mode first = getPin(pin).getMode();
+    Mode second = other.getPin(otherPin).getMode();
+    if (first == second
+        || first == Mode::UnusedMode
+        || second == Mode::UnusedMode)
+        throw ConnectionException();
+    try {
+        if (first == Mode::InputMode
+            || (first == Mode::DualMode && second == Mode::OutputMode))
+            _pins[pin].setConnection(other, otherPin);
+        else
+            other.getPin(otherPin).setConnection(*this, pin);
+    } catch (NoSuchPin &e) {
+        throw e;
+    }
+};
+
+nts::Pin &nts::AComponent::getPin(size_t pinNb)
+{
+    if (pinNb >= _nbPins)
+        throw NoSuchPin();
+    return _pins[pinNb];
+}
+
+nts::Tristate nts::AComponent::compute(std::size_t pin)
+{
+    if (pin >= this->_nbPins)
+        throw NoSuchPin();
+    return this->_pins[pin].getValue();
+}
+
+void nts::AComponent::simulate(std::size_t tick) noexcept
+{
+    auto iter = this->_pins.begin();
+    auto end = this->_pins.end();
+
+    if (this->_lastUpdateTick < tick)
+        this->_lastUpdateTick = tick;
+    else
+        return;
+    for (std::size_t i = 0; iter != end; ++i) {
+        std::optional<nts::Connection> &con = iter.base()->getConnection();
+        if ((iter.base()->getMode() == nts::Mode::InputMode
+            || iter.base()->getMode() == nts::Mode::DualMode)
+            && con.has_value()){
+            con.value().getComponent().get().simulate(tick);
+            auto value =
+                con.value().getComponent().get().compute(con.value().getPin());
+            this->_pins[i].setValue(value);
+        }
+        ++iter;
+    }
+    this->simulateComponent();
+}
+
+void nts::AComponent::resetOutputs(const nts::Tristate value)
+{
+    for (std::size_t i = 0; i < this->_nbPins; ++i)
+        if (this->_pins[i].getMode() == nts::Mode::OutputMode)
+            this->_pins[i].setValue(value);
+}

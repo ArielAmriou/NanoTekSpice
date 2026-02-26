814eb3cc1d9d6e4b6233f8d319fefdbf51f22634
Pierrick <pierrick.simon@epitech.eu>
2026-02-26 19:26:37 +0100

    fix: restore logic gate at there previus state

--- src/Components/C4Nor.cpp
+++ src/Components/C4Nor.cpp
@@ -14,13 +14,13 @@ nts::C4Nor::C4Nor() : AComponent() {
 
 void nts::C4Nor::simulateComponent() {
     this->_pins[2].setValue(
-        (this->_pins[0].getValue() / this->_pins[1].getValue()));
+        !(this->_pins[0].getValue() | this->_pins[1].getValue()));
     this->_pins[3].setValue(
-        (this->_pins[4].getValue() / this->_pins[5].getValue()));
+        !(this->_pins[4].getValue() | this->_pins[5].getValue()));
     this->_pins[9].setValue(
-        (this->_pins[7].getValue() / this->_pins[8].getValue()));
+        !(this->_pins[7].getValue() | this->_pins[8].getValue()));
     this->_pins[10].setValue(
-        (this->_pins[11].getValue() / this->_pins[12].getValue()));
+        !(this->_pins[11].getValue() | this->_pins[12].getValue()));
 }
 
 const std::vector<nts::Pin> nts::C4Nor::_defaultPins = {

ec0b3b4bf37f6026e0acdc51433dc53603e89f6e
Pierrick <pierrick.simon@epitech.eu>
2026-03-03 10:50:18 +0100

    feat(bonus): add new img

--- bonus/pictures/imageToRaw.py
+++ bonus/pictures/imageToRaw.py
@@ -7,18 +7,21 @@ parser.add_argument("input")
 parser.add_argument("output")
 args = parser.parse_args()
 
-img = Image.open(args.input).convert("RGB")
+img = Image.open(args.input).convert("RGBA")
 img_resized = img.resize((64, 64), Image.LANCZOS)
 pixels = np.array(img_resized)
 
 output = bytearray()
 for y in range(64):
     for x in range(64):
-        r, g, b = pixels[y, x]
+        r, g, b, a = pixels[y, x]
         r3 = int(r / 32) & 0x07
         g3 = int(g / 32) & 0x07
         b2 =  int(b / 64) & 0x03
-        value = (r3 << 5) | (g3 << 2) | b2
+        if a == 0:
+            value = 255
+        else:
+            value = (r3 << 5) | (g3 << 2) | b2
         output.append(value)
 
 with open(args.output, "wb") as f:

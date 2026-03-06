59b6f3d9b8127b5a302621a5101b47b87a3150f1
Pierrick <pierrick.simon@epitech.eu>
2026-03-06 07:10:02 +0100

    feat(bonus): add the components on the map

--- bonus/include/Map.hpp
+++ bonus/include/Map.hpp
@@ -13,6 +13,16 @@
 #include "Variables.hpp"
 
 namespace nts {
+
+    constexpr float SIZEMAP_X = WINDOW_SIZE_X / 10;
+    constexpr float SIZEMAP_Y = WINDOW_SIZE_Y / 10;
+    constexpr float SIZERATIO_X(float value) {return value * WINDOW_SIZE_X / (2 * MAXOFFSET_X + WINDOW_SIZE_X);}
+    constexpr float SIZERATIO_Y(float value) {return value * WINDOW_SIZE_Y / (2 * MAXOFFSET_Y + WINDOW_SIZE_Y);}
+    constexpr float POSRATIO_X(float value) {return value * SIZEMAP_X / (2 * MAXOFFSET_X + WINDOW_SIZE_X);}
+    constexpr float POSRATIO_Y(float value) {return value * SIZEMAP_Y / (2 * MAXOFFSET_Y + WINDOW_SIZE_Y);}
+    constexpr float OFFSETRATIO_X = 10 + SIZEMAP_X / 2;
+    constexpr float OFFSETRATIO_Y = WINDOW_SIZE_Y - 10 - SIZEMAP_Y / 2;
+
     class Map {
 
         public:
@@ -23,10 +33,11 @@ namespace nts {
 
         private:
             Variables &_var;
-            sf::Vector2f _sizeMap = {WINDOW_SIZE_X / 10, WINDOW_SIZE_Y / 10};
+            sf::Vector2f _sizeMap = {SIZEMAP_X, SIZEMAP_Y};
             sf::RectangleShape _recMap;
-            sf::Vector2f _sizeScreen = {_sizeMap.x * WINDOW_SIZE_X / (2 * MAXOFFSET_X + WINDOW_SIZE_X), _sizeMap.y * WINDOW_SIZE_Y / (2 * MAXOFFSET_Y + WINDOW_SIZE_Y)};
+            sf::Vector2f _sizeScreen = {SIZERATIO_X(_sizeMap.x), SIZERATIO_Y(_sizeMap.y)};
             sf::RectangleShape _recScreen;
+            sf::RectangleShape _recComponent;
     };
 }
 
--- bonus/src/Map.cpp
+++ bonus/src/Map.cpp
@@ -13,21 +13,37 @@ nts::Map::Map(Variables &var)
     _recMap.setOrigin(0, _sizeMap.y);
     _recMap.setPosition(10, WINDOW_SIZE_Y - 10);
     _recMap.setOutlineThickness(3);
-    _recMap.setOutlineColor(nts::RED);
-    _recMap.setFillColor(sf::Color::Transparent);
+    _recMap.setOutlineColor(sf::Color::Black);
+    _recMap.setFillColor(nts::LIGHTBLUE);
     _recScreen.setOrigin(_sizeScreen.x / 2, _sizeScreen.y / 2);
     _recScreen.setPosition(100, WINDOW_SIZE_Y - 100);
-    _recScreen.setFillColor(nts::RED);
+    _recScreen.setFillColor(sf::Color::Transparent);
+    _recScreen.setOutlineThickness(2);
+    _recScreen.setOutlineColor(nts::RED);
+    _recComponent.setFillColor(nts::DARKGREY);
 }
 
 void nts::Map::draw(sf::RenderWindow &window)
 {
     if (!_var._showMap)
         return;
+    window.draw(_recMap);
     _recScreen.setPosition(
-        10 + _sizeMap.x / 2 + _var._offset.x * _sizeMap.x / (2 * MAXOFFSET_X + WINDOW_SIZE_X),
-        WINDOW_SIZE_Y - 10 - _sizeMap.y / 2 + _var._offset.y * _sizeMap.y / (2 * MAXOFFSET_Y + WINDOW_SIZE_Y)
+        OFFSETRATIO_X + POSRATIO_X(_var._offset.x),
+        OFFSETRATIO_Y + POSRATIO_Y(_var._offset.y)
     );
     window.draw(_recScreen);
-    window.draw(_recMap);
+    for (auto iter = _var._components.begin(); iter != _var._components.end(); iter++) {
+        auto tmpSize = iter->second.first->getSize();
+        sf::Vector2f halfTmpSize = {tmpSize.x / 2, tmpSize.y / 2};
+        sf::Vector2f size = {tmpSize.x * _sizeScreen.x / WINDOW_SIZE_X, tmpSize.y * _sizeScreen.y / WINDOW_SIZE_Y};
+        auto tmpPos = iter->second.first->getPos() + halfTmpSize - sf::Vector2f(WINDOW_SIZE_X / 2, WINDOW_SIZE_Y / 2) - _var._offset;
+        _recComponent.setSize(size);
+        _recComponent.setOrigin(size.x / 2, size.y / 2);
+        _recComponent.setPosition(
+            OFFSETRATIO_X + POSRATIO_X(tmpPos.x),
+            OFFSETRATIO_Y + POSRATIO_Y(tmpPos.y)
+        );
+        window.draw(_recComponent);
+    }
 }

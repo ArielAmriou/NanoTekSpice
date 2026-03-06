fbd06754f82f6dfcbcc8f0b0757ef54507421077
Ariel <ariel.amriou@epitech.eu>
2026-03-06 03:19:35 +0100

    tests: add of 4040 and 4017 unit tests

--- tests/tests_component.cpp
+++ tests/tests_component.cpp
@@ -249,3 +249,25 @@ Test(TestFiles, CPN_shift, .init = redirect_all_std)
     fflush(stdout);
     cr_assert_stdout_eq_str(result.c_str());
 }
+
+Test(TestFiles, CPN_johnson, .init = redirect_all_std)
+{
+    std::string test = "4017";
+    std::string ext = test + ".nts";
+    
+    auto result = readFile(test.c_str(), true);
+    cr_assert_str_eq(test_main(ext.c_str(), test.c_str()), FINE);
+    fflush(stdout);
+    cr_assert_stdout_eq_str(result.c_str());
+}
+
+Test(TestFiles, CPN_counter, .init = redirect_all_std)
+{
+    std::string test = "4040";
+    std::string ext = test + ".nts";
+    
+    auto result = readFile(test.c_str(), true);
+    cr_assert_str_eq(test_main(ext.c_str(), test.c_str()), FINE);
+    fflush(stdout);
+    cr_assert_stdout_eq_str(result.c_str());
+}
